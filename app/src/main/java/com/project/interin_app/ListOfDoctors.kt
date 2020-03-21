7393b5374d7bea93054cc98bed83e90885525060
RSK-Robin <aasrobinen6989@mail.ru>
2020-03-21 19:46:14 +0300

    added list of doctors activity

---
+++ app/src/main/java/com/project/interin_app/ListOfDoctors.kt
@@ -0,0 +1,10 @@
+package com.project.interin_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ListOfDoctors : AppCompatActivity(R.layout.activity_listofdoctors) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
