532c7c3c5b3004a290e1ef4c5adf533b804c92b1
RSK-Robin <aasrobinen6989@mail.ru>
2020-06-10 15:54:47 +0300

    added display of active records

---
+++ app/src/main/java/com/project/interin_app/ui/userData/userRecord/UserRecordsAdapter.kt
@@ -0,0 +1,48 @@
+package com.project.interin_app.ui.userData.userRecord
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.project.interin_app.R
+import com.project.interin_app.repository.userData.Records
+
+class UserRecordsAdapter(var items: List<Records>, val itemClick: Listener) :
+    RecyclerView.Adapter<UserRecordsAdapter.RecordsViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : RecordsViewHolder {
+        val view =  LayoutInflater.from(parent.context).inflate(
+            R.layout.rv_list_of_user_records,
+            parent,
+            false
+        )
+        return RecordsViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: RecordsViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    inner class RecordsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(record: Records) {
+            val textViewName = itemView.findViewById(R.id.rv_records_name) as TextView
+            val textViewDate = itemView.findViewById(R.id.rv_records_date) as TextView
+            textViewName.text = record.Doctor
+            textViewDate.text = record.Date
+            itemView.setOnClickListener{
+                if(adapterPosition != RecyclerView.NO_POSITION){
+                    itemClick.onItemClick(items[adapterPosition])
+                }
+            }
+        }
+    }
+
+    interface Listener{
+        fun onItemClick(record: Records)
+    }
+}
--- app/src/main/java/com/project/interin_app/ui/userData/userRecord/UserRecordsFragment.kt
+++ app/src/main/java/com/project/interin_app/ui/userData/userRecord/UserRecordsFragment.kt
@@ -3,10 +3,36 @@ package com.project.interin_app.ui.userData.userRecord
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.project.interin_app.R
+import com.project.interin_app.repository.userData.Records
+import kotlinx.coroutines.launch
 
 class UserRecordsFragment : Fragment(R.layout.fragment_user_records) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val userRecordsViewModel by viewModels<UserRecordsViewModel>()
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            var content = userRecordsViewModel.getRecords()
+            if(content == null){
+                content = mutableListOf()
+            }
+            val recyclerView = view.findViewById<RecyclerView>(R.id.fud_rv_user_records)
+            recyclerView.layoutManager = LinearLayoutManager(activity)
+            recyclerView.adapter =
+                UserRecordsAdapter(
+                    content,
+                    object :
+                        UserRecordsAdapter.Listener {
+                        override fun onItemClick(record: Records) {
+
+                        }
+                    })
+        }
     }
 }
